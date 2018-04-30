5544564763f2bd97fd34cbfbe3663ceb3d4bd09a
speedy <speed.of.lightt@gmail.com>
2018-04-30 10:02:24 +0800

    show rgb/hex

--- src/components/landing-page.vue
+++ src/components/landing-page.vue
@@ -6,22 +6,51 @@
       v-model='style.background'
       placeholder='Givme a #hashbar'/>
     <p></p>
-    {{ raw }}
+    <div class='raw'>
+      <div> RGB(A): {{ raw.rgb }} </div>
+      <div> HEX: {{ raw.hex }} </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { debounce } from 'lodash'
 import { store } from '../store'
+
+function findTuples (str) {
+  return str.match(/\d{0,3}\.?\d+%?/g)
+}
+
+function rgb2Hex (rgb) {
+  const tup = findTuples(rgb).map(r => {
+    return parseFloat(r)
+  }).reduce((k, r) => {
+    const rs = r.toString(16)
+    return k + (rs.length === 1 ? ('0' + rs) : rs)
+  }, '')
+  return '#' + tup
+}
+
 export default {
   name: 'landing-page',
+  methods: {
+    updateRaw () {
+      let a = this.$el ? getComputedStyle(this.$el) : {}
+      const bg = a['background-color']
+      this.raw.rgb = bg
+      this.raw.hex = rgb2Hex(bg)
+      this.$forceUpdate()
+    }
+  },
   watch: {
     'style.background': debounce(function () {
-      let a = this.$el ? getComputedStyle(this.$el) : {}
-      console.log(a['background-color'])
-      this.raw.rgb = a['background-color']
-      this.$forceUpdate()
+      this.updateRaw()
     }, 400)
+  },
+  created () {
+    setTimeout(() => {
+      this.updateRaw()
+    }, 200)
   },
   data () {
     return {
